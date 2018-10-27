291e02f4f4793a26b4d8090ad447465614c26714
AMBAVLE <ambavleg@f45c89af2011.ant.amazon.com>
2018-10-27 23:11:47 +0530

    adding test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule,

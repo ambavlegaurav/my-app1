7a9f7591455a739c02e279cbadbedb7737737050
AMBAVLE <ambavleg@f45c89af2011.ant.amazon.com>
2018-10-27 23:56:13 +0530

    style binding Learned

--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Test2Component implements OnInit {
   public id ="hello "
-  public isDisabled = false
+  public isDisabled = true
     constructor() { }
 
   ngOnInit() {
